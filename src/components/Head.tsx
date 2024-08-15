909166d46ad59d7f309af8d00974dfdca48530b5
xocoee <141821998+xocoee@users.noreply.github.com>
2024-08-15 13:36:57 +0300

    update Head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -14,10 +14,10 @@ const Head: React.FC<HeadProps> = ({ isDarkTheme, toggleTheme, toggleLanguage })
   return (
     <div className="container-header">
       <div onClick={toggleLanguage} style={{ cursor: 'pointer' }}>
-        <h2>{t('head.language')}</h2>
+        <div className="text">{t('head.language')}</div>
       </div>
       <div onClick={toggleTheme} style={{ cursor: 'pointer' }}>
-        <h2>{isDarkTheme ? t('head.light') : t('head.dark')}</h2>
+        <div className="text">{isDarkTheme ? t('head.light') : t('head.dark')}</div>
       </div>
     </div>
   );
